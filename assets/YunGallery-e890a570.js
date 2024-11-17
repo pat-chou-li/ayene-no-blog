d73d809f46f8ef0721007d36e948f1f88328787f
pat-chou-li <pat-chou-li@users.noreply.github.com>
2024-11-17 17:23:03 +0000

    deploy: 80f4e85019b0a4f98d60017caac9c5986618b061

---
+++ assets/YunGallery-e890a570.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-41972f7e.js";import"./app-249fb4e9.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-5bc69db9.js";export{o as default};
---
+++ assets/_...all_-82da94e3.js
@@ -0,0 +1,1 @@
+import{f as a,a3 as s,u as n,a6 as r,o as c,h as p}from"./app-249fb4e9.js";import{b as t}from"./route-block-83d24a4e.js";const u=a({__name:"[...all]",setup(f){const o=s(),e=n();return r(()=>{o.push("/"),e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,l)=>(c(),p("div"))}});typeof t=="function"&&t(u);export{u as default};
